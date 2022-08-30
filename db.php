7a1e17f3932c1089b996764992b0bd2c0ccd4b29
marwa <marwakhalid558@gmail.com>
2022-08-30 12:47:02 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,2 +1,21 @@
-<?>
+<?php
+
+// Connecting to the Database
+$servername = "localhost";
+$username = "marwa";
+$password = "Marwa123@";
+$database = "Products";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo <script> console.log("Connection was successful")</script>;
+}
+
+?>
 
