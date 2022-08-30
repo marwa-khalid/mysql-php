27eae154729f98972d4da050c36fe0c7cce407ea
marwa <marwakhalid558@gmail.com>
2022-08-30 13:22:53 +0500

    Update db.php

--- db.php
+++ db.php
@@ -4,17 +4,28 @@
 $servername = "localhost";
 $username = "marwa";
 $password = "Marwa123@";
-$database = "Products";
 
 // Create a connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$connection = mysqli_connect($servername, $username, $password);
 
 // Die if connection was not successful
-if (!$conn){
+if (!$connection){
     die("Sorry we failed to connect: ". mysqli_connect_error());
 }
 else{
-    echo <script> console.log("Connection was successful")</script>;
+    echo '<script> console.log("Connection was successful")</script>';
+}
+
+// Create a db
+$sql = "CREATE DATABASE Product";
+$result = mysqli_query($connection, $sql);
+
+// Check for the database creation success
+if($result){
+    echo "The db was created successfully!";
+}
+else{
+    echo "The db was not created ".mysqli_error($connection);
 }
 
 ?>
