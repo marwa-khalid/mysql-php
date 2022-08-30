f6b5418663a31655a89a51aea19e2bfaf36ac50d
marwa <marwakhalid558@gmail.com>
2022-08-30 12:45:14 +0500

    connect
    connect to mysql

---
+++ products.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title>Shopping Site</title>
+        
+        <link rel="stylesheet" href="products.css">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="container"> 
+            <nav id="navbar" class="navbar navbar-expand-lg navbar-light fixed-top bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="products.html"><img src="logo.PNG" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myMenu"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="myMenu">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Sign in</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Privacy Policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="row mr-4 ml-4 mb-4">
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="cutetop.png" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Cute Pink Top</h5>
+                            <span class="price" >$15</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="s1.jfif" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Crop Top</h5>
+                            <span class="price" >$25</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="s3.jpg" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Tie Dye Shirt</h5>
+                            <span class="price" >$20</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-4">
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="vans.png" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Vans Old Skool</h5>
+                            <span class="price" >$50</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="shoes1.jpg" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >White Slip-on Sneakers</h5>
+                            <span class="price" >$70</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="shoe.jfif" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Jordan Retro Shoes</h5>
+                            <span class="price" >$150</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-4">
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="cutetop.png" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Cute Pink Top</h5>
+                            <span class="price" >$15</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="cutetop.png" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Cute Pink Top</h5>
+                            <span class="price" >$15</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 shadow m-4" >
+                    <div class="card-body">
+                        <img src="cutetop.png" class="image w-100"/>
+                        <div class="card-details text-center">
+                            <h5 class="name" >Cute Pink Top</h5>
+                            <span class="price" >$15</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer id="footer" class="footer bg-dark">
+            <div class="container">
+                 <div class="social">
+                        <h4></br>Contact Me</h4>
+                        <div class="social-links">
+                            <a target="_blank" href="https://web.facebook.com/marwa.khalid.12720/"><i class="fa fa-facebook"></i></a>
+                            <a target="_blank" href="https://twitter.com/__marwakhalid"><i class="fa fa-twitter"></i></a>
+                            <a target="_blank" href="https://www.instagram.com/marwakhalid___/"><i class="fa fa-instagram"></i></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/marwa-khalid-887804198/"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="footerbottom">
+                 <p>&copy;2022 All rights reserved</p>
+             </div>
+            </div>
+       </footer>
+    </body>
+</html>
